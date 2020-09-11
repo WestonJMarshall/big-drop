2aa945ba312ba311d9c43fe3008e283dab2d6408
wjm3824 <wjm3824@rit.edu>
2020-09-11 19:15:33 -0400

    Most of the coding done

--- Assets/Scripts/DropCube.cs
+++ Assets/Scripts/DropCube.cs
@@ -1,18 +1,88 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class DropCube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event EventHandler OnBlockRested;
+    public event EventHandler OnFailure;
+
+    public GameObject mouseEffect;
+
+    [Header("Block Falling Properties")]
+    [SerializeField]
+    private float swaySpeed = 0.5f;
+
+    [SerializeField]
+    private float swayDuration = 2.0f;
+
+    [SerializeField]
+    private float fallSpeedMultiplier = 1.015f;
+
+    private Rigidbody rb;
+    private float restTimer = 0.0f;
+    private float swapTimer = 0.0f;
+    private bool rlSwap = false;
+
+    public bool Falling { get; set; }
+
+    private void Awake()
     {
-        
+        Falling = true;
+        rb = GetComponentInChildren<Rigidbody>();
+
+        swaySpeed = UnityEngine.Random.Range(0.7f, 2.5f);
+        swayDuration = UnityEngine.Random.Range(1.9f, 4.5f);
+        fallSpeedMultiplier = UnityEngine.Random.Range(0.9985f, 1.002f);
+
+        swapTimer = swayDuration / 2.0f;
+
+        //GetComponentInChildren<MeshRenderer>().material.color = Color.HSVToRGB((Time.time / 400.0f) % 1, 1, 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Basically if the block is static for a little while, it tells the manager it is rested, manager will move on to the next block
+    /// </summary>
+    /// <returns>If the block has been static for long enough</returns>
+    public bool IsBlockRested()
     {
-        
+        restTimer = Convert.ToInt32(Mathf.Abs(rb.velocity.y) < 0.26f) * (restTimer + Time.deltaTime);
+        return restTimer > 0.9f ? true : false;
+    }
+
+    public void BlockRested()
+    {
+        OnBlockRested?.Invoke(this, null);
+    }
+
+    /// <summary>
+    /// Gives a block its swaying motion based on a set sway speed
+    /// </summary>
+    public void BlockFallingMovement()
+    {
+        swapTimer += Time.deltaTime;
+        if (swapTimer > swayDuration)
+        {
+            rlSwap = !rlSwap;
+            swapTimer = 0.0f;
+        }
+
+        float forceX = rlSwap ? swaySpeed : -swaySpeed;
+        rb.AddForceAtPosition(new Vector3(forceX, 0, 0), transform.position - new Vector3(0, 0.2f, 0));
+
+        rb.velocity = new Vector3(
+            rb.velocity.x,
+            rb.velocity.y / fallSpeedMultiplier,
+            rb.velocity.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Falling = false;
+
+        if(collision.gameObject.GetComponentInChildren<DropCube>() == null) //This is game over
+            OnFailure?.Invoke(this, null);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct GameStateVariables
+{
+    public float GameTime { get; set; }
+    public int CompletedBlockCount { get; set; }
+    public DropCube CurrentCube { get; set; }
+    public Vector3 DropLocation { get; set; }
+}
+
+public class GameManager : MonoBehaviour
+{
+    #region Singlton
+    public static GameManager Instance { get; private set; }
+    #endregion
+
+    private GameStateVariables gameVariables;
+
+    public GameObject cubePrefab;
+
+    private void Awake()
+    {
+        //Setup GameManager Singleton Instance
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        //Setup Game Variables
+        gameVariables = new GameStateVariables
+        {
+            GameTime = 0.0f,
+            CompletedBlockCount = 0,
+            CurrentCube = null,
+            DropLocation = new Vector3(0, 62, 100),
+        };
+    }
+
+    private void Start()
+    {
+        SpawnCube();
+    }
+
+    void Update()
+    {
+        if (gameVariables.CurrentCube == null)
+            return;
+
+        //Only check cube functions on current cube
+        if (gameVariables.CurrentCube.IsBlockRested())
+            gameVariables.CurrentCube.BlockRested();
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitData, 1000))
+        {
+            if (hitData.transform.gameObject.GetComponentInChildren<DropCube>() == gameVariables.CurrentCube) //Clicked on the current falling cube
+            {
+                gameVariables.CurrentCube.mouseEffect.SetActive(true);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    gameVariables.CurrentCube.Falling = false;
+                    gameVariables.CurrentCube.GetComponentInChildren<Rigidbody>().drag = 0.2f;
+                }
+            }
+            else
+            {
+                gameVariables.CurrentCube.mouseEffect.SetActive(false);
+            }
+        }
+        else
+        {
+            gameVariables.CurrentCube.mouseEffect.SetActive(false);
+        }
+
+        //Only run special cube movement on current cube
+        if (gameVariables.CurrentCube.Falling)
+        {
+            gameVariables.CurrentCube.BlockFallingMovement();
+
+        }
+    }
+
+    /// <summary>
+    /// Spawns a new drop cube at the current DropLocation
+    /// </summary>
+    private void SpawnCube()
+    {
+        if (cubePrefab != null)
+            gameVariables.CurrentCube = Instantiate(cubePrefab).GetComponent<DropCube>();
+            gameVariables.CurrentCube.mouseEffect.SetActive(false);
+            gameVariables.CurrentCube.transform.position = gameVariables.DropLocation;
+            gameVariables.CurrentCube.OnBlockRested += TopBlockRested;
+            gameVariables.CurrentCube.OnFailure += GameOver;
+    }
+
+    /// <summary>
+    /// Called when the block that was falling is now not falling and we can move on to the next
+    /// </summary>
+    private void TopBlockRested(object sender, System.EventArgs e)
+    {
+        gameVariables.CompletedBlockCount += 1;
+        Debug.Log(gameVariables.CompletedBlockCount);
+
+        gameVariables.CurrentCube.mouseEffect.SetActive(false);
+
+        Vector3 yDistIncrement = new Vector3(0, gameVariables.CurrentCube.transform.localScale.y, 0);
+        gameVariables.DropLocation += yDistIncrement;
+        StartCoroutine(nameof(LerpCameraPositionY), Camera.main.transform.position.y + yDistIncrement.y);
+
+        SpawnCube();
+    }
+
+    /// <summary>
+    /// Called on failure
+    /// </summary>
+    private void GameOver(object sender, System.EventArgs e)
+    {
+        Debug.Log("Game Over");
+    }
+
+    public IEnumerator LerpCameraPositionY(float pos)
+    {
+        yield return new WaitForEndOfFrame();
+
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x,
+            Camera.main.transform.position.y - ((Camera.main.transform.position.y - pos) * 0.015f),
+            Camera.main.transform.position.z);
+
+        if (Mathf.Abs(Camera.main.transform.position.y - pos) < 0.1f)
+        {
+            Camera.main.transform.position = new Vector3(
+            Camera.main.transform.position.x,
+            pos,
+            Camera.main.transform.position.z);
+        }
+        else
+        {
+            StartCoroutine(nameof(LerpCameraPositionY), pos);
+        }
+    }
+}
