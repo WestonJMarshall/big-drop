e10a3c0d3d5b194c8b6068a1c5432e788ea63f76
bhx4874 <bhx4874@g.rit.edu>
2020-09-14 15:59:15 -0400

    Pause Menu
    added a bugged pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+    public float restartDelay = 1.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        IsGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        IsGamePaused = true;
+    }
+
+    public void RestartDelay()
+    {
+        Invoke("Restart", restartDelay);
+        Debug.Log("delay");
+    }
+
+    public void Restart()
+    {
+        Debug.Log("restart");
+        Time.timeScale = 1.0f;
+        IsGamePaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
